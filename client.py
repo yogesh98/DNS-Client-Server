359407ca029131c6e98e5d5073106ac66e8b490b
Yugant Joshi <joshi.yugant@gmail.com>
2018-10-08 16:09:28 -0400

    data received from RSserver and TSserver

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import sys
 import socket as mysoc
 
-if len(sys.argv) != 6:
+if len(sys.argv) != 3:
     #Check if correct system arguments
-    print("invalid arguments, must provide hostnames.txt, RS hostname, RS port, TS hostname, TS port")
+    print("invalid arguments, must provide hostnames.txt, RS hostname")
     sys.exit()
 
 def initSockets():
@@ -14,37 +14,43 @@ def initSockets():
         print('{}\n'.format("socket open error", err))
     #setup RS
     rs_addr = sys.argv[2]
-    rs_port = int(sys.argv[3])
+    rs_port = 50007
     rs_server_binding = (rs_addr,rs_port)
-    client_socket.connect(rs_server_binding)
-    #setup TS
-    ts_addr = sys.argv[4]
-    ts_port = int(sys.argv[5])
-    ts_server_binding = (ts_addr,ts_port)
 
     fOut = open("RESOLVED.txt", "w+")
     fHostnames = open(sys.argv[1], "r")
     fHostnamesList = fHostnames.readlines()
-    connectToRS()
-
-def connectToRS():
-    print("connectToRS")
     for line in fHostnamesList:
         #Send each line to RS server
         print("[C:] sending:",line)
         client_socket.send(line.encode())
-        #Check recieved data has A or NS
-        ts_data_received = client_socket.recv(100)
-        resolved_entry = ts_data_received.decode('utf-8')
-        print("[C:] recieved", resolved_entry)
-        splicedEntry = resolved_entry[len(resolved_entry-2):]
+        #received data from RS
+        rs_data_received = client_socket.recv(100)
+        rs_resolved_entry = ts_data_received.decode('utf-8')
+        print("[C:] recieved", rs_resolved_entry)
+        #split string to determine A or NS
+        splicedEntry = rs_resolved_entry[len(resolved_entry-2):]
         splicedEntry.strip()
         #if A write to RESVOLVED.txt
-        #if NS go to TS server
-
-
-def connectToTS():
-    print("connectToTS")
-    #Pull TS address and port from system arguments
+        if splicedEntry == 'A':
+            print("[C:] Writing to RESOLVED:",rs_resolved_entry)
+            fOut.write("%s\n" % rs_resolved_entry)
+        #otherwise NS, go to TS server from resolved entry string
+        elif splicedEntry == 'NS':
+            #split on space, take s[0] as TS hostname
+            splitResolved = resolved_entry.split(" ")
+            ts_addr = splitResolved[0].trim()
+            ts_port = 50008
+            ts_server_binding = (ts_addr,ts_port)
+            client_socket.send(line.encode())
+            ts_data_received = client_socket.recv(100)
+            ts_resolved_entry = ts_data_received.decode('utf-8')
+            #If error is not found, write to RESOLVED.txt
+            if ts_resolved_entry.find("Error:") == -1:
+                fOut.write("%s\n" % ts_resolved_entry)
+            else:
+                print(ts_resolved_entry)
+    client_socket.close()
+    exit()
 
 initSockets()
