6bb658b8618ecb5ce89c13b14a46aef8be787e93
Yugant Joshi <joshi.yugant@gmail.com>
2018-10-09 12:09:04 -0400

    small fixes, implement file read-in for DNS entries

--- RSserver.py
+++ RSserver.py
@@ -1,4 +1,10 @@
+import sys
 import socket as mysoc
+
+if len(sys.argv) != 2:
+    #Check if correct system arguments
+    print("invalid arguments, must provide DNS entries file")
+    sys.exit()
 
 def RSserver():
     try:
@@ -7,19 +13,26 @@ def RSserver():
     except mysoc.error as err:
         print('{}\n'.format("RS socket open error",err))
 
-    rs_server_binding('', 50007)
-    rs_socket.bind(server_binding)
+    rs_server_binding=('', 50007)
+    rs_socket.bind(rs_server_binding)
     rs_socket.listen(1)
     hostname = mysoc.gethostname()
     print("[RS:] Server hostname:",hostname)
-    rs_host_ip = (mysoc.gethostbyname(host))
+    rs_host_ip = (mysoc.gethostbyname(hostname))
     print("[RS:] IP is",rs_host_ip)
-    csockid,addr=ss.accept()
+    csockid,addr=rs_socket.accept()
     print("[RS:] connection request from ",addr)
 
-    # TODO: store read-in file into array of strings
+    fDNSRSnames = open(sys.argv[1], "r")
+    fDNSRSList = fDNSRSnames.readlines()
+    inputEntries = []
+    for entry in fDNSRSList:
+        print("Storing: ",entry)
+        inputEntries.append(entry)
+
+
     # TODO: on acess - split on (" ") which separates string into array
-    # TODO: check first s_received[0] with s_table[0] for every string and check match  
+    # TODO: check first s_received[0] with s_table[0] for every string and check match
 
     #Read data from DNSRS and store in data structure
     #Receive data from client
